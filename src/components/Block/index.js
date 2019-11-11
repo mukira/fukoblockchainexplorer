dfce0e52b9e351308690c430e5d2d22ed979a269
rojdevensky@gmail.com <rojdevensky@gmail.com>
2019-11-11 12:57:35 +0300

    async getBlockState(block_hash) use web3 to get the Block object

--- src/components/Block/index.js
+++ src/components/Block/index.js
@@ -23,8 +23,23 @@ class Block extends Component {
   async componentWillReceiveProps(nextprops) {
     var block_hash_old = this.props.match.params.blockHash;
     var block_hash_new = nextProps.match.params.blockHash;
-
+    // compare old and new URL parameter (block hash)
+    // if different, reload state using web3
     if (block_hash_old !== block_hash_new)
     await this.getBlockState(block_hash_new);
   }
-}
+
+  async getBlockState(block_hash) {
+    // Use web3 to get the Block object
+    var currBlockObj = await web3_eth_getBlock(block_hash);
+    console.log(currBlockObj);
+    // Set the Component state
+    this.setState({
+      block_id: currBlockObj.number,
+      block_hash: currBlockObj.hash,
+      block_ts: moment.unix(parseInt(currBlockObj.timestamp, 10)),
+      block_txs: parseInt(currBlockObj.transactions.slice().length, 10),
+      block: currBlockObj
+    });
+  }
+
