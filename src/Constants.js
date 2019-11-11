7b140304c4a5ff4c1246016e2f4f2c2b4cb4d0bb
rojdevensky@gmail.com <rojdevensky@gmail.com>
2019-11-11 11:19:03 +0300

    added rinkeby.infura.io and alternative blockchain data provider

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,7 @@
 const Constants = {
-    PROVIDER: "https://mainnet.infura.io/kak6M2Qgf7oHycGaCI2E"
+    //PROVIDER: "https://mainnet.infura.io/kak6M2Qgf7oHycGaCI2E"
+    PROVIDER: "https://rinkeby.infura.io/kak6M2Qgf7oHycGaCI2E"
+
 }
 
 export default Constants;
