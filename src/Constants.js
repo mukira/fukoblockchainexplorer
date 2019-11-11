5b5e18186b703042f0416083171c09742cac826c
rojdevensky@gmail.com <rojdevensky@gmail.com>
2019-11-11 11:21:07 +0300

    added ropsten.infura.io as the blockchain data provider

--- src/Constants.js
+++ src/Constants.js
@@ -1,7 +1,9 @@
 const Constants = {
     //PROVIDER: "https://mainnet.infura.io/kak6M2Qgf7oHycGaCI2E"
     //PROVIDER: "https://rinkeby.infura.io/kak6M2Qgf7oHycGaCI2E"
-    PROVIDER: "https://kovan.infura.io/kak6M2Qgf7oHycGaCI2E"
+    //PROVIDER: "https://kovan.infura.io/kak6M2Qgf7oHycGaCI2E"
+    PROVIDER: "https://ropsten.infura.io/kak6M2Qgf7oHycGaCI2E"
+
 
 
 }
