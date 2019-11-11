34717ae9bed60c1ba5b4973f775a13cdc1599a1e
rojdevensky@gmail.com <rojdevensky@gmail.com>
2019-11-11 13:44:04 +0300

    render LatestBlocks

--- src/components/Stats/LatestBlocks/index.js
+++ src/components/Stats/LatestBlocks/index.js
@@ -24,3 +24,33 @@ class LatestBlocks extends Component {
       data: data
     })
   }
+  
+  render() {
+      var data = this.state.data;
+      var tableRows = [];
+      _.each(data.blocks, (block, index) => {
+        tableRows.push(
+          <tr key={block.number}>
+            <td className="tdCenter"><small>{block.number}</small></td>
+            <td><small><Link to={`/block/${block.hash}`}>{block.hash}</Link></small></td>
+          </tr>
+        )
+      });
+      return (
+          <div className="LatestBlocks">
+              <h2>Latest Blocks</h2>
+              <table>
+                <thead><tr>
+                  <th><i className="fa fa-cube"/> no.</th>
+                  <th>Hash</th>
+                </tr></thead>
+                <tbody>
+                  {tableRows}
+                </tbody>
+              </table>
+          </div>
+      );
+  }
+}
+
+export default LatestBlocks;
