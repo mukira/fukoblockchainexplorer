bbf14499d4acae5c2fb523416bb4964e6d8010a6
rojdevensky@gmail.com <rojdevensky@gmail.com>
2019-11-11 13:08:14 +0300

    render - this.state.transactions;

--- src/components/Block/TransactionList/index.js
+++ src/components/Block/TransactionList/index.js
@@ -28,3 +28,58 @@ class TransactionList extends Component {
       transactions: currListObj
     });
   }
+
+  render() {
+    var transactions = this.state.transactions;
+
+    if (!transactions){
+      return <pre>loading</pre>
+    }
+    var tableRows = [];
+    _.each(transactions, (value, index) => {
+      var txValue = 
+        parseInt(transactions[index].value, 10) / 1000000000000000000;
+      tableRows.push(
+        <tr key={transactions[index].hash}>
+          <td>
+            <Link to={`/tx/${transactions[index].hash}`}>
+              <small>{transactions[index].hash}</small>
+            </Link>
+          </td>
+          <td>
+            <Link to={`/address/${transactions[index].from}`}>
+              <small>{transactions[index].from}</small>
+            </Link>
+          </td>
+          <td>
+            <Link to={`/address/${transactions[index].to}`}>
+              <small>{transactions[index].to}</small>
+            </Link>
+          </td>
+          <td>{txValue}</td>
+        </tr>
+      );
+    });
+    return (
+      <div className="TransactionList">
+        <div className="card border-secondary mb-3">
+          <div className="card-header">Transactions</div>
+          <div className="card-body">
+            <Table striped responsive>
+              <thead>
+                <tr>
+                  <th>Tx Hash</th>
+                  <th>From</th>
+                  <th>To</th>
+                  <th>Value</th>
+                </tr>
+              </thead>
+              <tbody>{tableRows}</tbody>
+            </Table>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default TransactionList;
