7c7a081df7615f33aee1188d72cdc00622ac8d63
rojdevensky@gmail.com <rojdevensky@gmail.com>
2019-11-11 13:43:40 +0300

    LatestBlocks

---
+++ src/components/Stats/LatestBlocks/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import _ from 'lodash'
+import { Link } from 'react-router-dom'
+import './style.css'
+
+class LatestBlocks extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {}
+      }
+  }
+  
+  componentWillMount() {
+    // Get the block hash from URL arguments (defined by Route pattern)
+    this.getDataState(this.props.data);
+  }
+  
+  getDataState(data) {
+
+    // Set the Component state
+    this.setState({
+      data: data
+    })
+  }
