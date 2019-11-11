9f4355de0827ca30ead59a9081de7fc8de123c6b
rojdevensky@gmail.com <rojdevensky@gmail.com>
2019-11-11 10:58:55 +0300

    added web3.currentProvider for blockchain data

--- src/web3Helpers.js
+++ src/web3Helpers.js
@@ -1,3 +1,25 @@
 import Web3 from 'web3';
 
 import Constants from './Constants'
+
+Let provider;
+
+if (window.web3 && window.web3.currentProvider) {
+  provider = window.web3.provider;
+} else {
+  provider = new Web3.providers.HttpProvider(Constants.PROVIDER);
+}
+
+export const web3 = new Web3(provider);
+
+export const web3_eth_getBlockNumber = () => {
+  return new Promise((resolve, reject) => {
+    web3.eth.getBlockNumber((err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  });
+};
+
