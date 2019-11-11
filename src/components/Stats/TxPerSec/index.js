a90438c46fa1490e096a692624250e1a2263d130
rojdevensky@gmail.com <rojdevensky@gmail.com>
2019-11-11 13:48:09 +0300

    TxPerSec

---
+++ src/components/Stats/TxPerSec/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import './style.css'
+import _ from 'lodash'
+
+class TxPerSec extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {}
+      }
+  }
+  
+  componentWillMount() {
+    // Get the block hash from URL arguments (defined by Route pattern)
+    this.getDataState(this.props.data);
+  }
+  
+  getDataState(data) {
+
+    // Set the Component state
+    this.setState({
+      data: data
+    })
+  }
