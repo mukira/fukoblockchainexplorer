aa5dd9c19df87c64a95eb049df6c60bd5e466202
rojdevensky@gmail.com <rojdevensky@gmail.com>
2019-11-11 13:22:18 +0300

    render AvgBlockTime

--- src/components/Stats/AvgBlockTime/index.js
+++ src/components/Stats/AvgBlockTime/index.js
@@ -21,3 +21,40 @@ class AvgBlockTime extends Component {
       data: data
     });
   }
+
+  render() {
+    var data = this.state.data.blocks;
+    var times = [];
+
+    _.each(data, (value, index) => {
+      if (index !== data.length - 1)
+        times.push(data[index].timestamp - data[index + 1].timestamp);
+    });
+
+    if (!times.length) {
+      return <pre>error</pre>;
+    }
+    
+    const add = (a, b) => a + b;
+    // use reduce to sum our array
+    const sum = times.reduce(add);
+    var average = Math.round(sum / times.length);
+
+    return (
+      <div className="AvgBlockTime">
+        <div className="card border-secondary mb-3">
+          <div className="card-header" style={{ textAlign: 'center' }}>
+            Avg Block Time
+          </div>
+          <div className="card-body">
+            <h1 className="card-title" style={{ textAlign: 'center' }}>
+              <i className="fas fa-stopwatch" /> {average}s
+            </h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AvgBlockTime;
