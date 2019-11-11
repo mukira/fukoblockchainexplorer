03faa335bcb3e4a47d6a450fbe19b620bd1c8e6f
rojdevensky@gmail.com <rojdevensky@gmail.com>
2019-11-11 13:18:42 +0300

    added github corner to github link

---
+++ src/components/Home/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './style.css'
+import Stats from './../Stats';
+import GithubCorner from 'react-github-corner';
+
+class Home extends Component {
+
+  render() {
+      return (
+          <div>
+            <div className="Home container">
+                <Stats/>
+            </div>
+            <div>
+                <GithubCorner href="https://github.com/mukira/fukoblockchainexplorer" octoColor="#303030" bannerColor="#00bc8c" direction="left" />
+            </div>
+        </div>
+      );
+  }
+}
+
+export default Home;
