ee2ab9dbc917c2d34ec7c51d6b802e6c03540eff
rojdevensky@gmail.com <rojdevensky@gmail.com>
2019-11-16 16:07:56 +0300

    formatted code

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
-import './style.css';
-import Block from './../Block';
-import Transaction from './../Transaction';
-import Address from './../Address';
-import Home from './../Home';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
-import { Text,IconWallet } from '@aragon/ui'
-
-
+import React, { Component } from "react";
+import "./style.css";
+import Block from "./../Block";
+import Transaction from "./../Transaction";
+import Address from "./../Address";
+import Home from "./../Home";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import createHistory from "history/createBrowserHistory";
+import { Text, IconWallet } from "@aragon/ui";
 
 import {
   Collapse,
@@ -28,14 +26,14 @@ import {
   Input,
   FormGroup,
   Button
-} from 'reactstrap';
+} from "reactstrap";
 
-import { web3_eth_getTransactionByHash } from '../../web3Helpers';
+import { web3_eth_getTransactionByHash } from "../../web3Helpers";
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchValue: ''
+      searchValue: ""
     };
 
     // This binding is necessary to make `this` work in the callback
@@ -46,59 +44,57 @@ class App extends Component {
     const history = createHistory({ forceRefresh: true });
     if (this.state.searchValue.length === 42) {
       // address
-      history.push('/address/' + this.state.searchValue);
+      history.push("/address/" + this.state.searchValue);
     } else if (this.state.searchValue.length === 66) {
       // block or tx
       if (await web3_eth_getTransactionByHash(this.state.searchValue)) {
         //transaction
-        history.push('/tx/' + this.state.searchValue);
+        history.push("/tx/" + this.state.searchValue);
       } else {
         // block
-        history.push('/block/' + this.state.searchValue);
+        history.push("/block/" + this.state.searchValue);
       }
     } else {
-      console.log('nothing');
+      console.log("nothing");
     }
   }
 
   render() {
     return (
       <div className="App">
-        <Navbar className="navbar-absolute fixed-top navbar-transparent  navbar navbar-expand-lg bg-transparent" >
-             <a className="navbar-brand" href="/">
-                <i className="fab fa-ethereum" /> Fuko Blockchain Explorer
-              </a>
-            
+        <Navbar className="navbar-absolute fixed-top navbar-transparent  navbar navbar-expand-lg bg-transparent">
+          <a className="navbar-brand" href="/">
+            <i className="fab fa-ethereum" /> Fuko Blockchain Explorer
+          </a>
+
           <Nav className="justify-content-end collapse navbar-collapse">
             <NavItem>
-            <form>
-            <InputGroup className="no-border">
-                  <Input placeholder
+              <form>
+                <InputGroup className="no-border">
+                  <Input
+                    placeholder
                     type="text"
                     placeholder="Tx, Block or Address"
                     value={this.state.searchValue}
                     onChange={evt => this.updateInputValue(evt)}
                     onKeyPress={e => {
-                      if (e.key === 'Enter') {
+                      if (e.key === "Enter") {
                         this.search();
                       }
                     }}
                   />
-                                  <InputGroupAddon addonType="append">
-
-                <InputGroupText>
-                <IconWallet onClick={this.search} />
-                </InputGroupText>
-                </InputGroupAddon>
+                  <InputGroupAddon addonType="append">
+                    <InputGroupText>
+                      <IconWallet onClick={this.search} />
+                    </InputGroupText>
+                  </InputGroupAddon>
                 </InputGroup>
-                </form>
-    
-
+              </form>
             </NavItem>
           </Nav>
         </Navbar>
         <div className="App-nav">
-          {' '}
+          {" "}
           {
             <Router>
               <div>
